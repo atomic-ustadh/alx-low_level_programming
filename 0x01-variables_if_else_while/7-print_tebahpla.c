d7227faa87b3a27fb4fb6dc7370fa740903b2a11
atomic-ustadh <ibnluqmanalawfaaweey@gmail.com>
2022-10-15 17:21:51 -0700

    Comit Group 4.0

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "time.h"
+/**
+ * Main - Entry Level
+ * Return: Always 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i >= 'a'; i--)
+	putchar(i);
+	putchar('\n');
+	return (0);
+}
